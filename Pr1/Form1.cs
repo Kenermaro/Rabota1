5fd174be5053e77777b47525b797d6136f6d5a0a
dddh1 <pkear98@bk.ru>
2023-09-12 09:31:36 +0400

    Vr 1.1

---
+++ Pr1/Form1.cs
@@ -0,0 +1,20 @@
+namespace Pr1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
